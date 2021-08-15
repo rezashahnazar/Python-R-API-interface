d91b9f1e1ac8a9f5b13db1ff327a3b643db5f744
Reza <reza.shahnazar@gmail.com>
2021-08-16 01:51:04 +0430

    readme

--- API.py
+++ API.py
@@ -8,6 +8,9 @@ app = Flask(__name__, template_folder=".")
 
 @app.route("/", methods=["post", "get"])
 def Home():
+    # change the following url (and port) with whatever your plumber-R api is listening on.
+    url = "http://127.0.0.1:7767/"
+
     msg = ""
     resp = ""
     resPlt = ""
@@ -24,9 +27,9 @@ def Home():
             sexStat = "woman"
         msg = f"In a {age} year old {sexStat} with {wt} Kgs wieght, creatinin level of {cr} makes a GFR estimation of:"
 
-        apiUrl = "http://127.0.0.1:7767/gfr"
+        apiUrl = f"{url}gfr"
         body = {"age": age, "sex": sex, "wt": wt, "cr": cr}
         response = requests.post(apiUrl, data=body).json()
         resp = round(response[0], 1)
-        image = f"http://127.0.0.1:7767/plt?age={age}&sex={sex}&wt={wt}&cr={cr}"
+        image = f"{url}plt?age={age}&sex={sex}&wt={wt}&cr={cr}"
     return render_template("gfr-tmp.html", message=msg, response=resp, image=image)
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # R-plumber-Python-flask
 
 A simple API connection between R & Python using plumber & flask libraries.
+
+1. run th API in GFR.R
+2. from R-studio console, copy the _url and port_ which your api is listening on.
+3. run flask with the following commands:
+
+- export FLASK_APP=API
+- run flask
